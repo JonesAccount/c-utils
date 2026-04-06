a3864d2142db3eb7a420683e9a51b0de8d0486b1
Jones. <jonesmailhub@gmail.com>
2026-04-06 15:22:57 +0500

    Update min.c

--- min.c
+++ min.c
@@ -1,1 +1,12 @@
-n
+int min(int *arr, size_t size) {
+        if (size == 0) { return INT_MAX; }
+
+        int result = arr[0];
+
+        for (size_t i = 1; i < size; i++) {
+                if (arr[i] < result) {
+                        result = arr[i];
+                }
+        }
+
+        return result;
