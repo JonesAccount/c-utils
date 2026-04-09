86979b6d72aec52498788c1ac23f78cdeec57c65
Jones. <jonesmailhub@gmail.com>
2026-04-09 09:59:16 +0500

    Create getch.c

---
+++ getch.c
@@ -0,0 +1,11 @@
+char getch(void) {
+    struct termios oldt, newt;
+    char ch;
+    tcgetattr(STDIN_FILENO, &oldt);       
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);    
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt); 
+    return ch;
+}
